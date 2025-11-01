b7d20474fc4ce4cfb33ac814cdbdc88255cc2294
Nikita <nekit9401@yandex.ru>
2025-11-01 12:04:59 +0300

    feat: implemented getData func

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-console.log('Hello');
+const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
+
+const getData = async <T>(url: string): Promise<T> => {
+	try {
+		const res: Response = await fetch(url);
+		if (!res.ok) throw new Error(`HTTP ${res.status}: ${res.statusText}`);
+
+		const data: T = await res.json();
+
+		return data;
+	} catch (error) {
+		if (error instanceof Error) {
+			console.error(error.message);
+		}
+
+		throw error;
+	}
+};
+
+interface Post {
+	userId: number;
+	id: number;
+	title: string;
+	body: string;
+}
+
+getData<Post[]>(POSTS_URL)
+	.then((posts) => {
+		posts.forEach((post) => {
+			console.log(`ID: ${post.id}, Title: ${post.title}`);
+		});
+	})
+	.catch((error) => {
+		console.error('Ошибка при загрузке постов:', error);
+	});
